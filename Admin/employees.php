ccb55b0b6c6a0207c1e2651278327c541f35440a
NikTheCompiler <72276649+NikTheCompiler@users.noreply.github.com>
2022-03-01 14:04:52 +0200

    Add files via upload

---
+++ Admin/employees.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Employees</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../assets/img/favicon.png" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin - v2.1.0
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="dashboard.php" class="logo d-flex align-items-center">
+        <img src="../assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">SPS Security</span>
+      </a>
+      
+    </div><!-- End Logo -->
+
+    
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item d-block d-lg-none">
+          <a class="nav-link nav-icon search-bar-toggle " href="#">
+            <i class="bi bi-search"></i>
+          </a>
+		  
+        </li><!-- End Search Icon-->
+
+        <li class="nav-item dropdown pe-3">
+
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Kevin Anderson</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="profile.php">
+                <i class="bi bi-person"></i>
+                <span>My Profile</span>
+              </a>
+            </li>
+            
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="dashboard.php">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="Department1.php">
+          <i class="bi bi-card-list"></i>
+          <span>Department 1</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="Department2.php">
+          <i class="bi bi-card-list"></i>
+          <span>Department 2</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="employees.php">
+          <i class="bi bi-layout-text-sidebar"></i>
+          <span>Employees</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="questions.php">
+          <i class="bi bi-question-lg"></i>
+          <span>Questions</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="profile.php">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+
+      
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Employees</h1>
+      <nav>
+        
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-10">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Datatables</h5>
+              <p> <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank"></a>  <code></code> </p>
+				
+				<!-- Basic Modal -->
+				<p align="right">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#smallModal">
+                Add Employee
+              </button>
+              <div class="modal fade" id="smallModal" tabindex="-1">
+                <div class="modal-dialog modal-sm">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">Add Employee</h5>
+                    </div>
+					
+                    <div class="modal-body">
+                      Full Name
+                    </div>
+					<center><input size="30"></input></center>
+					<div class="modal-body">
+                      Position
+                    </div>
+					<center><input size="30"></input></center>
+					<div class="modal-body">
+                      Country
+                    </div>
+					<center><input size="30"></input></center>
+					<div class="modal-body">
+                      Address
+                    </div>
+					<center><input size="30"></input></center>
+					<div class="modal-body">
+                      Phone
+                    </div>
+					<center><input size="30"></input></center>
+					<div class="modal-body">
+                      Email
+                    </div>
+					<center><input size="30"></input></center><br>
+				
+                    <div class="modal-footer">
+						<button type="button" class="btn btn-primary">Add</button>
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+					
+                  </div>
+                </div>
+              </div><!-- End Basic Modal-->
+			<p align="right">
+              <!-- Table with stripped rows -->
+              <table class="table datatable">
+			  
+                <thead>
+				
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+					<th scope="col">Department</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">ID</th>
+                    <th scope="col"></th>
+					<th scope="col"></th>
+					<th scope="col"></th>
+					<th scope="col"></th>
+                  </tr>
+                </thead>
+				
+                <tbody>
+				
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Brandon Jacob</td>
+					<td>Department 1</td>
+                    <td>Designer</td>
+                    <td>28</td>
+                    <td style="color:blue">Remove</td>
+					<td style="color:blue">Update</td>
+					<td style="color:blue">Details</td>
+					<td style="color:blue">Generate Password</td>
+					
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Bridie Kessler</td>
+					<td>Department 2</td>
+                    <td>Developer</td>
+                    <td>35</td>
+                    <td style="color:blue">Remove</td>
+					<td style="color:blue">Update</td>
+					<td style="color:blue">Details</td>
+					<td style="color:blue">Generate Password</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Ashleigh Langosh</td>
+					<td>Department 3</td>
+                    <td>Finance</td>
+                    <td>45</td>
+                    <td style="color:blue">Remove</td>
+					<td style="color:blue">Update</td>
+					<td style="color:blue">Details</td>
+					<td style="color:blue">Generate Password</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>Angus Grady</td>
+					<td>Department 4</td>
+                    <td>HR</td>
+                    <td>34</td>
+                    <td style="color:blue">Remove</td>
+					<td style="color:blue">Update</td>
+					<td style="color:blue">Details</td>
+					<td style="color:blue">Generate Password</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">5</th>
+                    <td>Raheem Lehner</td>
+					<td>Department 2</td>
+                    <td>Dynamic Division Officer</td>
+                    <td>47</td>
+                    <td style="color:blue">Remove</td>
+					<td style="color:blue">Update</td>
+					<td style="color:blue">Details</td>
+					<td style="color:blue">Generate Password</td>
+                  </tr>
+                </tbody>
+				
+              </table>
+			  
+              <!-- End Table with stripped rows -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete fe links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/quill/quill.min.js"></script>
+  <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="../assets/vendor/chart.js/chart.min.js"></script>
+  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="../assets/vendor/echarts/echarts.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
