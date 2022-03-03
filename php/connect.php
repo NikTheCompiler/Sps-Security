a6a41c9e2c86f5fee96ec3fddf8c3b6b92b280ff
NikTheCompiler <nikolaki_2000@hotmail.com>
2022-03-03 13:24:12 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,14 +1,46 @@
-<?php
-$servername = "DESKTOP-FCR3KJC";
-$username = "sps";
-$password = "sps";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=SpsSecurity", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+<?php   
+$server_nm="DESKTOP-FCR3KJC";
+$connection=array("Database"=>"SpsSecurity","UID"=>"","PWD"=>"");
+$conn= sqlsrv_connect($server_nm,$connection);
+if($conn){
+   //echo "Connection established!";
 }
-?>
+else{
+   echo"Connection could not be established";
+   die(print_r(sqlsrv_errors(),true));
+}
+?>     
+
+<?php  
+/*
+ Specify the server and connection string attributes.   
+$serverName = "DESKTOP-FCR3KJC";  
+$connectionInfo = array( "Database"=>"SpsSecurity");  
+  
+ Connect using Windows Authentication.   
+$conn = sqlsrv_connect( $serverName, $connectionInfo);  
+if( $conn === false )  
+{  
+     echo "Unable to connect.</br>";  
+     die( print_r( sqlsrv_errors(), true));  
+}  
+  
+ Query SQL Server for the login of the user accessing the  
+database.   
+$tsql = "SELECT CONVERT(varchar(32), SUSER_SNAME())";  
+$stmt = sqlsrv_query( $conn, $tsql);  
+if( $stmt === false )  
+{  
+     echo "Error in executing query.</br>";  
+     die( print_r( sqlsrv_errors(), true));  
+}  
+  
+ Retrieve and display the results of the query.  
+$row = sqlsrv_fetch_array($stmt);  
+echo "User login: ".$row[0]."</br>";  
+  
+ Free statement and connection resources.   
+sqlsrv_free_stmt( $stmt);  
+sqlsrv_close( $conn);
+*/  
+?>  
