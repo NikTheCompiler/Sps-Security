4a9289d7b72298b7bd5822c145c2e3038fde7815
andronikos98 <andronikos.evangelou@gmail.com>
2022-03-12 14:49:52 +0200

    new user

--- php/newuser.php
+++ php/newuser.php
@@ -6,8 +6,8 @@ $admin=3;
 $secretary=2;
 $manager=1;
 $user=0;
-$pass = $_POST["pass"];
 
+$pass = $_POST["pass"];
 $new_input = $_POST["newpass"];
 $new = strip_tags($new_input);
 
@@ -25,7 +25,8 @@ if ($password== $pass){
     //$new = hash("sha256", $new);
     $sql = "UPDATE Users SET password='".$new."' , newuser = '1' WHERE username = '".$user."'";
     if (sqlsrv_query($conn,$sql)){
-      if($_SESSION['type']== $admin){
+      if($_SESSION['type']== $admin)
+      {
           $_SESSION['surname'] = $row['surname'];
           $_SESSION['name'] = $row['name'];
           $_SESSION['email'] = $row['email'];
@@ -33,12 +34,11 @@ if ($password== $pass){
           $_SESSION['username'] = $username;
           $_SESSION['position'] = $row['position'];
           $_SESSION['UserID'] = $row['UserID'];
-          $_SESSION['newuser'] = $row['newuser'];
-          $_SESSION['password'] = $password;
           header('Location: ../Admin/dashboard.php');
         }
 
-      else if($_SESSION['type']==$secretary){
+      else if($_SESSION['type']==$secretary)
+      {
           $_SESSION['surname'] = $row['surname'];
           $_SESSION['name'] = $row['name'];
           $_SESSION['email'] = $row['email'];
@@ -48,7 +48,8 @@ if ($password== $pass){
           $_SESSION['UserID'] = $row['UserID'];
           header('Location: ../Secretary/dashboard.php');
       }
-      else if($_SESSION['type']== $manager){
+      else if($_SESSION['type']== $manager)
+      {
           $_SESSION['surname'] = $row['surname'];
           $_SESSION['name'] = $row['name'];
           $_SESSION['email'] = $row['email'];
