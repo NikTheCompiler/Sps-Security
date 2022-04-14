e2818c5b8eee9c2ca099d41acd9bcc3d8a41916d
NikTheCompiler <nikolaki_2000@hotmail.com>
2022-04-14 14:16:58 +0300

    dashboards

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -237,7 +237,7 @@ Secure(3);
 
         <!-- Left side columns -->
 
-<div class="col-lg-6">
+<div class="col-lg-12">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Average grade for each Department in the last 2 months</h5>
@@ -312,54 +312,7 @@ Secure(3);
 			  </div>
           </div>
         </div>
-<!-- Bar Chart -->
-<div class="col-lg-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Average grade of each SubSection of Questions</h5>
 
-              <canvas id="chart" style="max-height: 400px;"></canvas>
-              <script>
-                document.addEventListener("DOMContentLoaded", () => {
-                  new Chart(document.querySelector('#chart'), {
-                    type: 'bar',
-                    data: {
-                      labels: ['SubSection 1', 'SubSection 2', 'SubSection 3', 'SubSection 4'],
-                      datasets: [{
-                        label: 'Average grades',
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(255, 159, 64, 0.2)',
-                          'rgba(255, 205, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(201, 203, 207, 0.2)'
-                        ],
-                        borderColor: [
-                          'rgb(255, 99, 132)',
-                          'rgb(255, 159, 64)',
-                          'rgb(255, 205, 86)',
-                          'rgb(75, 192, 192)',
-                          'rgb(54, 162, 235)',
-                          'rgb(153, 102, 255)',
-                          'rgb(201, 203, 207)'
-                        ],
-                        borderWidth: 1
-                      }]
-                    },
-                    options: {
-                      scales: {
-                        y: {
-                          beginAtZero: true
-                        }
-                      }
-                    }
-                  });
-                });
-              </script>
-              <!-- End Bar Chart -->
 
                 </div>
 
